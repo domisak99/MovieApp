b3b6efac1c344a791b1708b9372de41414affc63
d_palan <d_palan@utb.cz>
2022-01-25 09:46:05 +0100

    Úprava search options

--- src/app/search-options/search-options.page.ts
+++ src/app/search-options/search-options.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Storage } from '@capacitor/storage';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-search-options',
@@ -7,8 +10,12 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./search-options.page.scss'],
 })
 export class SearchOptionsPage implements OnInit {
-
-  constructor(public modalController: ModalController) { }
+  soType: string
+  soYear: any
+  constructor(public modalController: ModalController) {
+    this.getType();
+    this.getYear();
+  }
 
   ngOnInit() {
   }
@@ -17,4 +24,33 @@ export class SearchOptionsPage implements OnInit {
     this.modalController.dismiss();
   }
 
+  async setItem() {
+    if (this.soYear != null) {
+      var year = moment(this.soYear).format('YYYY');
+      console.log(year);
+      await Storage.set({
+        key: 'year',
+        value: JSON.stringify(year),
+      });
+    }
+    if (this.soType != null) {
+      await Storage.set({
+        key: 'type',
+        value: this.soType,
+      });
+    }
+    this.modalController.dismiss();
+  };
+
+  async getType() {
+    const { value } = await Storage.get({ key: 'type' });
+    //this.type = value;
+    this.soType = value;
+    //console.log(this.soType);
+  };
+  async getYear() {
+    const { value } = await Storage.get({ key: 'year' });
+    this.soYear = parseInt(value);
+  };
 }
+
